fc0205c97ce420324037834f1a78058d1d432b57
Luis Rodrigues <larpig@gmail.com>
2022-06-24 18:32:09 -0300

    Graph | Adding methods to add node and edge

--- OrientedGraph.py
+++ OrientedGraph.py
@@ -1,22 +1,34 @@
 class Graph:
-    def __init__(self, edges):
+    def __init__(self, nodes, edges):
+        self.nodes = nodes
         self.edges = edges
-        self.adj_list = self.edges_2_adjacency_list(edges)
+        self.adj_list = self.edges_2_adjacency_list(nodes, edges)
 
     @staticmethod
-    def edges_2_adjacency_list(edges):
+    def edges_2_adjacency_list(nodes, edges):
+
         adj_list = {}
+        for node in nodes:
+            adj_list[node] = []
+
         for start_node, end_node in edges:
-            if start_node not in adj_list:
-                adj_list[start_node] = [end_node]
-            else:
-                adj_list[start_node].append(end_node)
+            adj_list[start_node].append(end_node)
+
         return adj_list
+
+    def add_node(self, node):
+        self.nodes.append(node)
+        self.adj_list = self.edges_2_adjacency_list(self.nodes, self.edges)
+        return True
+
+    def add_edge(self, start, end):
+        self.edges.append((start, end))
+        self.adj_list = self.edges_2_adjacency_list(self.nodes, self.edges)
+        return True
 
     def get_paths(self, start, end, path=[]):
 
         path = path + [start]
-        #path.append(start)
 
         if start == end:
             return [path]
@@ -35,6 +47,8 @@ class Graph:
 
 
 if __name__ == '__main__':
+    cities = ["Mumbai", "Paris", "Dubai", "New York", "Toronto", ]
+
     routes = [
         ("Mumbai", "Paris"),
         ("Mumbai", "Dubai"),
@@ -45,11 +59,17 @@ if __name__ == '__main__':
         ("New York", "Toronto"),
     ]
 
-    print(f"Adjacency list representation:", Graph.edges_2_adjacency_list(routes))
+    print(f"Adjacency list representation:", Graph.edges_2_adjacency_list(cities, routes))
 
-    route_graph = Graph(routes)
+    route_graph = Graph(nodes=cities, edges=routes)
+
+    route_graph.add_node('São Paulo')
+    print(f"All nodes after add São Paulo:", route_graph.nodes)
+
+    route_graph.add_edge('São Paulo', "New York")
+    print(f"All edges after add (São Paulo, New York):", route_graph.edges)
 
     start = "Mumbai"
     end = "Dubai"
     print(f"All paths between {start} and {end}: ", route_graph.get_paths(start, end))
-    #print(f"Shortest path between {start} and {end}: ", route_graph.get_shortest_path(start, end))
+    # print(f"Shortest path between {start} and {end}: ", route_graph.get_shortest_path(start, end))
